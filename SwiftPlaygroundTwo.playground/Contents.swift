15b064a58966411c837eb27c4e917d39fe731c2d
Kaili Kameoka <kaili.kameoka@gmail.com>
2022-01-31 15:48:22 -1000

    Completed parts 1 - 4

--- SwiftPlaygroundTwo.playground/Contents.swift
+++ SwiftPlaygroundTwo.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
 var greeting = "Hello, playground"
+
+// Part 2
+var goodGrade = "I've been doing all my work";
+if goodGrade == "I've been doing all my work" {
+    print("I should be getting an A at this point")
+} else {
+    // Part 3
+    print("I need to work harder in this class.")
+}
+
+// Part 4
+
+var gradeA = true;
+if (gradeA == true) {
+    print("I really like this class")
+} else {
+    print("I'm not so sure about coding in Swift")
+}
